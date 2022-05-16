fa8b6d9c9ee84705391d2189109065207a9c1fbf
Johan ten Houten <46577547+johantenhouten@users.noreply.github.com>
2022-05-16 23:14:51 +0200

    Update ultrasoon-1.py

--- ultrasoon-1.py
+++ ultrasoon-1.py
@@ -1,16 +1,29 @@
-# werkt voor nu alleen met https://makecode.microbit.org/#editor
-# daar kan je extra biblitheek invoeren
+#
+# werkt voor  https://makecode.microbit.org/#editor
+#
+# voor het berekenen van afstand moet eerst de grove uitbreiding worden geladen
+# daarna zijn grove functies beschikbaar.
+#
 
-#
-#
+# 
+# functie die de ultrasoon sensor op de grove kit uitleest
+# in dit voorbeeld is de ultrasoon sensor in PIN 1 geplaatst.
+# let op dat PIN1 niet voor iets anders wordt gebruikt
+# 
+# de grove kit wordt niet in de online omgeving gesimuleerd.
 def afstand():
-     return grove.measure_in_centimeters_v2(DigitalPin.P1)
+    # de functie grove.measure_in_centimeters_v2 geeft een antwoord 
+    # als een decimaal breuk (3.45 betekent 3 centimeter en 4.5 millimeter)
+    # geeft als afstand het aantal decimeters
+    cm = grove.measure_in_centimeters_v2(DigitalPin.P1) 
+
+    # deel het aantal centimeters door 10 en neem alleen het hele gtal
+    return int(cm/10)
 
 
-def on_forever():
-    basic.show_number(afstand())
-    music.play_tone(Note.C, music.beat())
-    basic.pause(1000)
-
-
-basic.forever(on_forever)
+while True:
+    # als de afstand onder de 50cm komt laat een schedel zien, anders veeg scherm leeg
+    if afstand() < 5:
+        basic.show_icon(IconNames.SKULL)
+    else:
+        basic.clear_screen()
