5ad97240decd84100abb6dd106ee09d3eb7bed58
Johan ten Houten <46577547+johantenhouten@users.noreply.github.com>
2022-05-16 16:35:16 +0200

    Radio
    Voorbeeld code voor het zenden  ontvangen van data tussen 2 microbits.

---
+++ radio_zenden_ontvangen.py
@@ -0,0 +1,23 @@
+
+radio.set_group(1)
+
+def on_button_pressed_a():
+    radio.send_number(0)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    radio.send_number(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+
+def on_received_number(receivedNumber):
+    basic.show_number(receivedNumber)
+    if receivedNumber == 0:
+        basic.show_icon(IconNames.HEART)
+    elif receivedNumber == 1:
+        basic.show_icon(IconNames.SKULL)
+    else:
+        basic.clear_screen()
+
+radio.on_received_number(on_received_number)
+
